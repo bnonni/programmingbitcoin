a046840379ee0d690e42bd87ed7131f16d4183f8
Bryan Nonni <bn185068@ncr.com>
2021-11-12 12:35:46 -0500

    Chapter2

--- code-ch02/ecc.py
+++ code-ch02/ecc.py
@@ -155,33 +155,51 @@ class Point:
         if self.a != other.a or self.b != other.b:
             raise TypeError('Points {}, {} are not on the same curve'.format(self, other))
 
+        # P1 is the point at infinity
         if self.x is None:  # <3>
             return other
+
+        # P2 is the point at infinity
         if other.x is None:  # <4>
             return self
         # end::source3[]
 
         # Case 1: self.x == other.x, self.y != other.y
+        # x = x, y ≠ y results in I or infinity point
         # Result is point at infinity
         if self.x == other.x and self.y != other.y:
             return self.__class__(None, None, self.a, self.b)
 
         # Case 2: self.x ≠ other.x
+        # x ≠ x, formula for point addition changes
         # Formula (x3,y3)==(x1,y1)+(x2,y2)
         # s=(y2-y1)/(x2-x1)
         # x3=s**2-x1-x2
         # y3=s*(x1-x3)-y1
-        s = (other.y - self.y) / (other.x - self.x)
-        x = s**2 - self.x - other.x
-        y = s * (self.x - x) - self.y
-        return self.__class__(x, y, self.a, self.b)
+        if self.x != other.x:
+            s = (other.y - self.y) / (other.x - self.x)
+            x = s**2 - self.x - other.x
+            y = s * (self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
 
+        # Case 4: P1 == P2 and y1 = 0
+        # results in s w/ 0 denominator
+        if self == other and self.y == 0 * self.x:
+            return self.__class__(None, None, self.a, self.b)
+        
         # Case 3: self == other
+        # P1 == P2, formula for point addition changes
         # Formula (x3,y3)=(x1,y1)+(x1,y1)
         # s=(3*x1**2+a)/(2*y1)
         # x3=s**2-2*x1
         # y3=s*(x1-x3)-y1
-
+        if self == other:
+            s = (3 * self.x**2 + self.a) / (2 * self.y)
+            x = s**2 - 2 * self.y
+            y = s * (self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
+        
+        
 
 class PointTest(TestCase):
 
